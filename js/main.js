615ee6867377454c2451278be2a2d197bfd74474
RanmaOVA <Masseymichael643@gmail.com>
2022-01-15 23:45:10 -0600

    added js, html, css

--- js/main.js
+++ js/main.js
@@ -31,5 +31,50 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /* Accordion skills */
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((eL) =>{
+    eL.addEventListener('click', toggleSkills)
+})
 
 /* Qualification Tabs */
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
+
+/* Services Modal*/
+
+/* Portfolio Swiper */
+
+/* Testimonal */
+
+/* Scroll sections active link*/
+
+/* Change Background Header */
